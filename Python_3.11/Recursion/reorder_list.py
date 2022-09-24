dcadcc62687ebf66487f0c3746ea551856a64dcd
Egon Fiedler <egon@Egons-MacBook-Air.local>
2022-09-24 10:38:52 -0400

    Only missing piece is to make it so depending on whether it is of even or un even lenght the node stack is appended at the right position

--- Python_3.11/Recursion/reorder_list.py
+++ Python_3.11/Recursion/reorder_list.py
@@ -13,6 +13,11 @@
         
         - When you use recursion, every node goes into the call stack, then it is about using that structure ...
             => 
+            
+        - I must figure out when I must start switching value and under what rule,
+        
+        
+        - I must start appending to the other stack once I have the stack size until it reaches after which I start appending the nodes in the other stack into the linkedlist 
         
 Diagramming + Logic, 
 
@@ -70,17 +75,27 @@ Stack,
 (4)=>
 
 Understanding case scenarios, when its 8 nodes
+#pre recursive call
+counter  1        2        3        4        5        6        7        8        
+        (1)=>    (2)=>    (3)=>    (4)=>    (5)=>    (6)=>    (7)=>    (8)=>   None
+                                      
+self.stack_count =  8//2-1 => 3 **I append the last 3 elements using this count into the stack
 
-    (1)=>    (2)=>    (3)=>    (4)=>    (5)=>    (6)=>    (7)=>    (8)=>   None
-           ^=                                                      <=
 
-    (1)=>    (8)=>    (2)=>    (3)=>    (4)=>    (5)=>    (6)=>    (7)=>   None
-                             ^=                                    <=
-    
-    (1)=>    (8)=>    (2)=>    (7)=>    (3)=>    (4)=>    (5)=>    (6)=>   None
-                                               ^=                  <=
 
-    (1)=>    (8)=>    (2)=>    (7)=>    (3)=>    (6)=>    (4)=>    (5)=>   None
+#post recursive call, after having taken elements out into the stack and stack count being 0
+
+      (1)=>      (2)=>      (3)=>      (4)=>      (5)=> 
+         (8)=>None  (7)=>None  (6)=>None
+#now I append 
+
+
+                        ]
+               (6)=>None     
+               (7)=>None 
+self.stack = [ (8)=>None
+
+expected = [1,8,2,7,3,6,4,5]
 
 
 Understanding case scenarios, when its 5 nodes
@@ -95,20 +110,26 @@ Understanding case scenarios, when its 5 nodes
                                                
 
 Understanding case scenarios, when its 9 nodes
+#pre recursive call
+counter  1        2        3        4        5        6        7        8       9 
+        (1)=>    (2)=>    (3)=>    (4)=>    (5)=>    (6)=>    (7)=>    (8)=>   (9)=>   None
+                                      ^=         <=
+self.stack_count =  4 **I append the last 4 elements using this count into the stack
 
-    (1)=>    (2)=>    (3)=>    (4)=>    (5)=>    (6)=>    (7)=>    (8)=>   (9)=>   None
-           ^=                                                              <=
+#post recursive call, after having taken elements out into the stack and stack count being 0
 
-    (1)=>    (9)=>    (2)=>    (3)=>    (4)=>    (5)=>    (6)=>    (7)=>   (8)=>   None
-                             ^=                                            <=
-    
-    (1)=>    (9)=>    (2)=>    (8)=>    (3)=>    (4)=>    (5)=>    (6)=>   (7)=>   None
-                                               ^=                          <=
+      (1)=>      (2)=>      (3)=>      (4)=>      (5)=> 
+         (9)=>None  (8)=>None  (7)=>None  (6)=>None
+#now I append 
 
-    (1)=>    (9)=>    (2)=>    (8)=>    (3)=>    (7)=>    (4)=>    (5)=>   (6)=>   None
-                                                                 ^=        <=
-                                                                 
-    (1)=>    (9)=>    (2)=>    (8)=>    (3)=>    (7)=>    (4)=>    (6)=>   (5)=>   None
+
+                    ]
+               (6)=>None     
+               (7)=>None     
+               (8)=>None 
+self.stack = [ (9)=>None
+        
+expected =  [1,9,2,8,3,7,4,6,5]   
     
 Solution 1 "Using the call stack from the recursive call and another stack"
 **Making the pattern for even and un-even
@@ -180,9 +201,12 @@ class Solution:
         if num1 % 2 == 0:
             print("10 is even") => this one prints
         '''
-        def helper(node, counter):
+        def helper(node, prev_node, counter):
             if node is None: 
                 return 
+
+            print("pre recursive call")
+            print("this is the counter == ", counter)
             
             if node.next is None:  
                 print("\n")
@@ -194,21 +218,42 @@ class Solution:
                     self.stack_count = counter // 2
                     print("self.stack_count = ",self.stack_count)
                     print("\n")
-            print("pre recursive call")
-            print("this is the counter == ", counter)
-            helper(node.next, counter+1)
+
+            helper(node.next, prev_node.next, counter+1)
             print("\n")
             print("post recursive call")
             print("this is the counter == ", counter)
             
-            # I must get the stack to have the last 1 value
-            if self.stack_count > 0:
+            # This 
+            if self.stack_count >= 0:
                 #if counter 
-                print("This is top value before decreasing should be 1 == ",self.stack_count)
-                self.stack_count -= 1
-                print("This is top value after decreasing should be 0 == ",self.stack_count)
-            
-        
-        helper(head, 1)
+                if self.stack_count == 0:
+                    node.next = None
+                    self.stack_count -= 1
+                else:
+                    print("This is top value before decreasing == ",self.stack_count)
+                    node.next = None
+                    self.stack.append(node)
+                    print(self.stack)
+                    self.stack_count -= 1
+                    print("This is top value after decreasing == ",self.stack_count)
+    
+            #When it is an even array expected = [1,8,2,7,3,6,4,5] this is the result, with 8 nodes
+            ##When it is an uneven array expected =  [1,9,2,8,3,7,4,6,5] this is the result, with 9 nodes
+            else:
+                print("This is the current node ", node.val, "this is the previous node ", prev_node.val, "this is the node.next ",node.next)
+
+                stack_node = self.stack.pop()
+                print("this is stack_node ",stack_node)
+                
+                '''this looks out for even or uneven for when to add, otherwise
+                
+                stack_node.next = node
+                prev_node.next = stack_node
+                
+                '''
+                
+        helper(head.next, head, 2)
 
         return 
+        
